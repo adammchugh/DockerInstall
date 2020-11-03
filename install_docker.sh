1cecfaf876b45ac9c109cd06a3fb2ba211a8aa5e
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-11-03 15:33:42 +1030

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,7 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo systemctl status docker
 sudo docker run hello-world
 sudo usermod -aG docker $USER
